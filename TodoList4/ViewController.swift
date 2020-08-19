4421e6f2846978c1e11d9c1f697c7baf9f107769
Wladmir <wlad.w.junior@gmail.com>
2020-08-19 17:37:46 -0300

    up project

--- TodoList4/ViewController.swift
+++ TodoList4/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  
+  var listaDeTarefas: [String] = []
+  let listaKey = "chaveLista"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    tableView.dataSource = self
+    
+    if let lista = UserDefaults.standard.value(forKey: listaKey) as? [String] {
+      listaDeTarefas.append(contentsOf: lista)
+      tableView.reloadData()
+    }
   }
+    
+  @IBAction func addTask(_ sender: Any) {
+    let alert = UIAlertController(title: "Nova tarefa",
+                                  message: "Adicione uma nova tarefa",
+                                  preferredStyle: .alert)
+    
+    let acaoSalvar = UIAlertAction(title: "Salvar", style: .default) { (action) in
+      
+      if let textField = alert.textFields?.first, let texto = textField.text {
+        self.listaDeTarefas.append(texto)
+        UserDefaults.standard.set(self.listaDeTarefas, forKey: self.listaKey)
+        self.tableView.reloadData()
+      }
+    }
+    
+    let acaoCancel = UIAlertAction(title: "Cancelar", style: .cancel)
+    
+    alert.addAction(acaoSalvar)
+    alert.addAction(acaoCancel)
+    
+    alert.addTextField()
+    
+    present(alert, animated: true)
+  }
+}
 
-
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return listaDeTarefas.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = listaDeTarefas[indexPath.row]
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      listaDeTarefas.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+      UserDefaults.standard.set(self.listaDeTarefas, forKey: self.listaKey)
+    }
+  }
 }
 
